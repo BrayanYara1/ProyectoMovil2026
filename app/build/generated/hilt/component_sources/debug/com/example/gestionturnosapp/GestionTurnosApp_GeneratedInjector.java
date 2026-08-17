4ac4b83feb790da412d6b38080b464fc3b7c1e41
Brahiam yara <ayara-2022a@corhuila.edu.co>
2026-08-17 15:19:49 -0500

    docs: restaurar backlog y ADR tras sincronización con repo oficial

---
+++ app/build/generated/hilt/component_sources/debug/com/example/gestionturnosapp/GestionTurnosApp_GeneratedInjector.java
@@ -0,0 +1,17 @@
+package com.example.gestionturnosapp;
+
+import dagger.hilt.InstallIn;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.components.SingletonComponent;
+import dagger.hilt.internal.GeneratedEntryPoint;
+import javax.annotation.processing.Generated;
+
+@OriginatingElement(
+    topLevelClass = GestionTurnosApp.class
+)
+@GeneratedEntryPoint
+@InstallIn(SingletonComponent.class)
+@Generated("dagger.hilt.android.processor.internal.androidentrypoint.InjectorEntryPointGenerator")
+public interface GestionTurnosApp_GeneratedInjector {
+  void injectGestionTurnosApp(GestionTurnosApp gestionTurnosApp);
+}
